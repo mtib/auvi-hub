a0d5cd32feebd0518ad234b5229aff36087d855b
Markus Becker <markus@tibyte.net>
2015-09-01 17:38:54 +0200

    Neue Tex-Datei. Umstrukturierung,

---
+++ Code/PyVi/www/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Jugend Forscht - AuVi</title>
+	<?	
+		include("parts/header.php");
+	?>
+</head>
+<body>
+<!-- Files at ../web/ORT/PARA/* -->
+This is the PHP file: <? echo "Test"; ?>
+</body>
+</html>
