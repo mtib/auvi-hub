c95da97445e9ad7e1d217627d414656132949ef0
unknown <markus.becker@R316-PC13.sz-kborn.local>
2015-09-24 11:48:14 +0200

    Whitespace after command

---
+++ .gource/run.bat
@@ -0,0 +1,1 @@
+..\..\..\Portable\Gource\gource.exe .. --camera-mode track --user-image-dir .user -1280x720 -a 2 -e 0.1 -b 000000 -title "Jugend forscht - AuVi" --multi-sampling -max-files 0 -hide bloom,date,dirnames,filenames,progress -r 60 
