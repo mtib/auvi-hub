909d44e266a825f67b437b37e62d472257d0f0d5
Markus Becker <markus@tibyte.net>
2015-09-29 17:02:54 +0200

    cleanup

--- Latex/Schulpreis/installFullLatex.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-apt-get install texlive
-apt-get install texlive-luatex
-apt-get install texlive-latex-base
-apt-get install texlive-fonts-recommended
-apt-get install texlive-lang-german
-apt-get install texmaker
